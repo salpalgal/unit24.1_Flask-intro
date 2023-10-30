e524eee941f01eb97b4058ef871aada109b38317
sally wang <sal@LAPTOP-66V3CAS9.localdomain>
2023-10-30 11:51:39 -0700

    combine all math functions to one

--- calc/app.py
+++ calc/app.py
@@ -3,30 +3,8 @@ from flask import Flask, request
 from operations import add ,sub, mult , div
 
 app = Flask(__name__)
-@app.route("/math/add")
-def adding_math():
-    a = int(request.args["a"])
-    b= int(request.args["b"])
-    sum = add(a,b)
-    return f"<h1>{sum}</h1>"
-@app.route("/math/sub")
-def subract_math():
-    a = int(request.args["a"])
-    b= int(request.args["b"])
-    difference = sub(a,b)
-    return f"<h1>{difference}</h1>"
-@app.route("/math/mult")
-def multiply_math():
-    a = int(request.args["a"])
-    b= int(request.args["b"])
-    product = mult(a,b)
-    return f"<h1>{product}</h1>"
-@app.route("/math/div")
-def divide_math():
-    a = int(request.args["a"])
-    b= int(request.args["b"])
-    quotient = div(a,b)
-    return f"<h1>{quotient}</h1>"
+
+
 
 @app.route("/add")
 def adding():
@@ -52,5 +30,19 @@ def divide():
     b= int(request.args["b"])
     quotient = div(a,b)
     return f"<h1>{quotient}</h1>"
+
+
+
+
+
+@app.route("/math/<mathing>")
+
+def all_math(mathing):
+    
+    a = int(request.args["a"])
+    b= int(request.args["b"])
+    math ={"add":add(a,b),"sub":sub(a,b),"mult":mult(a,b),"div":div(a,b)}
+    toMath = math[mathing]
+    return f"<h1>{toMath}</h1>"
 if __name__ == "__main__":
     app.run(FLASK_ENV="environement")
